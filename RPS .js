2b4ed8c5e0c5e1902bae38aec9af32fd4ed3496d
Kozmit <bkoznarek@gmail.com>
2023-10-16 11:45:25 -0500

    Correct 'playRound' Function to Compare Properly

--- RPS .js
+++ RPS .js
@@ -12,49 +12,52 @@ function getComputerChoice() {
      }
 return computerSelection;
 }
-
+//let playerSelection = prompt('Rock, Paper, or Scissors?')
+//create function with answers for each combination rock paper scissors
 function playRound() {
-      if (computerSelection = 'Rock') {
-         if (playerSelection = 'Rock') {
-            print('Rock ties with Rock!');
+      if (computerSelection == 'Rock') {
+         if (playerSelection == 'Rock') {
+            return 'Rock ties with Rock!';
          }
-         if (playerSelection = 'Paper') {
-            print('Paper beats Rock! You Win!');
+         if (playerSelection == 'Paper') {
+            return 'Paper beats Rock! You Win!';
          }
-         if (playerSelection = 'Scissors') {
-            print('Scissors looses to Rock! You Lose!');
-         }
-      }
-      if (computerSelection = 'Paper') {
-         if (playerSelection = 'Rock') {
-            print('Rock looses to Paper! You Lose!');
-         }
-         if (playerSelection = 'Paper') {
-            print('Paper ties with Paper! Try Again!');
-         }
-         if (playerSelection = 'Scissors') {
-            print('Scissors beats Paper! You Win!');
+         if (playerSelection == 'Scissors') {
+            return 'Scissors looses to Rock! You Lose!';
          }
       }
-      if (computerSelection = 'Scissors') {
-         if (playerSelection = 'Rock') {
-            print('Rock beats Scissors! You Win!');
+      if (computerSelection == 'Paper') {
+         if (playerSelection == 'Rock') {
+            return 'Rock looses to Paper! You Lose!';
          }
-         if (playerSelection = 'Paper') {
-            print ('Paper looses to Scissors! You Lose!')
+         if (playerSelection == 'Paper') {
+            return 'Paper ties with Paper! Try Again!';
          }
-         if (playerSelection = 'Scissors') {
-            print('Scissors ties with Scissors! Try Again!')
+         if (playerSelection == 'Scissors') {
+            return 'Scissors beats Paper! You Win!';
+         }
+      }
+      if (computerSelection == 'Scissors') {
+         if (playerSelection == 'Rock') {
+            return 'Rock beats Scissors! You Win!';
+         }
+         if (playerSelection == 'Paper') {
+            return 'Paper looses to Scissors! You Lose!';
+         }
+         if (playerSelection == 'Scissors') {
+            return 'Scissors ties with Scissors! Try Again!';
          }
       }
       else {
-         print('That was not an option given! Try again');
+         return 'That was not an option given! Try again';
       }
 }
 
-let playerSelection = prompt('Rock, Paper, or Scissors?')
+
+const playerSelection = 'Rock';
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 //prompt user to type 'Rock' 'Paper' or 'Scissors'
 //make user input be case insensative
-//create answers for each combination rock paper scissors
 //return to user whether win lose or tie
 //prompt user wheather to play again or not
