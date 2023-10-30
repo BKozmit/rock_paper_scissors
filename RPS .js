041265e16556f760581a0625b3240fc5886da116
Kozmit <bkoznarek@gmail.com>
2023-10-30 13:41:53 -0500

    Create and Properly Invoke 'game' Function

--- RPS .js
+++ RPS .js
@@ -18,7 +18,7 @@ let playerSelection = prompt("Rock, Paper, or Scissors?");
 let computerSelection = getComputerChoice();
 
 //create function with answers for each combination rock paper scissors and return to user whether win lose or tie
-function playRound(computerSelection, playerSelection) {
+function playRound() {
       if (computerSelection == 'Rock') {
          if (playerSelection == 'Rock') {
             return 'Rock ties with Rock!';
@@ -57,11 +57,33 @@ function playRound(computerSelection, playerSelection) {
       }
 }
 
-console.log(playRound(computerSelection, playerSelection));
-//function game() {
-   //playerSelection = prompt("Rock, Paper, or Scissors?");
-   //computerSelection = getComputerChoice();
-   //console.log(playRound(playerSelection, computerSelection));
-//}
+function game() {
+   let playerSelection = prompt("Rock, Paper, or Scissors?");
+   let computerSelection = getComputerChoice();
+   playRound(playerSelection, computerSelection);
+   console.log(playRound(playerSelection, computerSelection));
+
+   playerSelection = prompt("Rock, Paper, or Scissors?");
+   computerSelection = getComputerChoice();
+   playRound(playerSelection, computerSelection)
+   console.log(playRound(playerSelection, computerSelection));
+
+   playerSelection = prompt("Rock, Paper, or Scissors?");
+   computerSelection = getComputerChoice();
+   playRound(playerSelection, computerSelection);
+   console.log(playRound(playerSelection, computerSelection));
+
+   playerSelection = prompt("Rock, Paper, or Scissors?");
+   computerSelection = getComputerChoice();
+   playRound(playerSelection, computerSelection);
+   console.log(playRound(playerSelection, computerSelection));
+
+   playerSelection = prompt("Rock, Paper, or Scissors?");
+   computerSelection = getComputerChoice();
+   playRound(playerSelection, computerSelection);
+   console.log(playRound(playerSelection, computerSelection));
+}
+
+game();
 //make user input be case insensative
 //prompt user wheather to play again or not
